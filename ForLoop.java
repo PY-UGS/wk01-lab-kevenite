129b06dcab1dd720d867a32b581720b4ae4688f9
Malvin Tok <kevenitetok@gmail.com>
2022-01-19 13:12:12 +0800

    Added For Loop to loop decrement from 102 to 66

---
+++ ForLoop.java
@@ -0,0 +1,8 @@
+public class ForLoop{
+    public static void main(String[] args) {
+        for (int x = 102; x > 65; x = x-1){
+            System.out.print("Value of x: " + x);
+            System.out.print("\n");
+        }
+    }
+}
